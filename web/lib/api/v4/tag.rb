b1ce963050f09160e111b76f90cd1b38f2cfa409
Jochen Topf <jochen@topf.org>
2026-05-21 15:10:02 +0200

    Fix: Return nil instead of failing to devide by zero

--- web/lib/api/v4/tag.rb
+++ web/lib/api/v4/tag.rb
@@ -87,7 +87,7 @@ class Taginfo < Sinatra::Base
                 :other_key      => row['other_key'],
                 :other_value    => row['other_value'],
                 :together_count => row['together_count'].to_i,
-                :to_fraction    => (row['together_count'].to_f / has_this_key.to_f).round(4),
+                :to_fraction    => has_this_key ? (row['together_count'].to_f / has_this_key.to_f).round(4): nil,
                 :from_fraction  => row['from_fraction'].to_f.round(4)
             }
             end
